db2c1040a619e8c3f534c065bd8ccc5cd93f8925
Kevin Blanchard <blanchardkevinj@gmail.com>
2021-05-11 20:40:45 -0400

    Fixing up text UI

--- Stages/StartingScreen/MainMenuStage.cs
+++ Stages/StartingScreen/MainMenuStage.cs
@@ -8,6 +8,7 @@
 using Microsoft.Xna.Framework;
 using SgEngine.EKS;
 using SgEngine.GUI;
+using SgEngine.GUI.Components;
 
 namespace MultiplayerZelda.Stages.StartingScreen
 {
@@ -16,15 +17,48 @@ namespace MultiplayerZelda.Stages.StartingScreen
         public override void Initialize()
         {
             base.Initialize();
-            var MainMenuUiPanel = new Panel();
-            var UiIcon = new GuiUiImage(ZeldaGraphics.BasicUiSquare, new Point(128, 128), GameWorld.WorldCenter.ToVector2());
-            MainMenuUiPanel.AddUiObject(UiIcon);
+            var spaceBetweenTextBoxes = 20;
+            var borders = 8;
+            var MainMenuUiPanel = new Panel(new Vector2(200, 100), new Point(100, 100), ZeldaGraphics.BasicUiSquare);
+            TextBoxConfig newGameTextConfig = new TextBoxConfig
+            {
+                alignment = GuiTextComponent.Alignment.Left,
+                displayText = "New Game",
+                fontType = GuiTextComponent.FontTypes.ChronoTypeRegular,
+                parent = MainMenuUiPanel,
+                parentOffset = new Vector2(borders,borders),
+                textBoxSize = new Point(80,10)
+            };
+            TextBoxConfig continueTextConfig = new TextBoxConfig
+            {
+                alignment = GuiTextComponent.Alignment.Left,
+                displayText = "Continue",
+                fontType = GuiTextComponent.FontTypes.ChronoTypeRegular,
+                parent = MainMenuUiPanel,
+                parentOffset = new Vector2(borders,borders+spaceBetweenTextBoxes),
+                textBoxSize = new Point(80,10)
+            };
+            TextBoxConfig optionsTextBox = new TextBoxConfig
+            {
+                alignment = GuiTextComponent.Alignment.Left,
+                displayText = "Options",
+                fontType = GuiTextComponent.FontTypes.ChronoTypeRegular,
+                parent = MainMenuUiPanel,
+                parentOffset = new Vector2(borders,borders+spaceBetweenTextBoxes*2),
+                textBoxSize = new Point(80,10)
+            };
+            var NewGameText = new GuiTextComponent(newGameTextConfig);
+            var ContinueGameText = new GuiTextComponent(continueTextConfig);
+            var OptionsText = new GuiTextComponent(optionsTextBox);
+            MainMenuUiPanel.AddUiObject(NewGameText);
+            MainMenuUiPanel.AddUiObject(ContinueGameText);
+            MainMenuUiPanel.AddUiObject(OptionsText);
             GameWorld.Gui.MasterCanvas.AddPanel(MainMenuUiPanel);
         }
 
         public override void Update(GameTime gameTime)
         {
-            
+
 
         }
     }
