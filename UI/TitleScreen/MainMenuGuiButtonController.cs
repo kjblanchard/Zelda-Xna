04447152f64b7b53e7fda28477b579e366aaa0c2
Kevin Blanchard <blanchardkevinj@gmail.com>
2021-05-22 13:13:14 -0400

    Refactored guibuttonController

--- UI/TitleScreen/MainMenuGuiButtonController.cs
+++ UI/TitleScreen/MainMenuGuiButtonController.cs
@@ -6,7 +6,6 @@
 ////////////////////////////////////////////////////////////
 
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using SgEngine.GUI.Components;
 using SgEngine.GUI.Types;
 
@@ -14,18 +13,13 @@ namespace MultiplayerZelda.UI.TitleScreen
 {
     public class MainMenuGuiButtonController : GuiButtonController
     {
-        public GuiImageComponent CursorGuiImageComponent;
         public MainMenuGuiButtonController(GuiComponent parent, Vector2 offset = new Vector2(), Point size = new Point()) : base(parent, offset, size)
         {
-        }
-
-        public override void Initialize()
-        {
-            base.Initialize();
             CursorGuiImageComponent =
                 new GuiImageComponent(this, ZeldaGraphics.RightPointFingerCursor, new Point(16, 16))
                 {
-                    LocationOverride = true
+                    LocationOverride = true,
+                    IsVisible = false
                 };
         }
 
@@ -33,12 +27,6 @@ namespace MultiplayerZelda.UI.TitleScreen
         {
             base.Update(gameTime);
             CursorGuiImageComponent.LocalPosition = UpdateCursorPosition();
-        }
-
-        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-        {
-            base.Draw(gameTime, spriteBatch);
-            CursorGuiImageComponent.Draw(gameTime,spriteBatch);
         }
         private Vector2 UpdateCursorPosition()
         {
