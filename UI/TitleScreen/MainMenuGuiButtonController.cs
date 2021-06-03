0f5030d443ba5a154bba154490633b938a61fb24
Kevin Blanchard <blanchardkevinj@gmail.com>
2021-06-03 17:11:05 -0400

    Refactored button controller to hold the key events

--- UI/TitleScreen/MainMenuGuiButtonController.cs
+++ UI/TitleScreen/MainMenuGuiButtonController.cs
@@ -5,17 +5,22 @@
 //
 ////////////////////////////////////////////////////////////
 
+using System;
 using Microsoft.Xna.Framework;
 using MultiplayerZelda.Utils.Enums;
 using SgEngine.Core.Input;
 using SgEngine.GUI.Components;
 using SgEngine.GUI.Types;
 using SgEngine.Interfaces.Input;
+using SgEngine.Interfaces.Sound;
 
 namespace MultiplayerZelda.UI.TitleScreen
 {
-    public class MainMenuGuiButtonController : GuiButtonController,ISubscribeToButton.IHandleAButtonPressed, ISubscribeToButton.IHandleUpButtonPressed, ISubscribeToButton.IHandleDownButtonPressed
+    public class MainMenuGuiButtonController : GuiButtonController, IHandleAButtonPressed, IHandleUpButtonPressed, IHandleDownButtonPressed, IPlaySfx
     {
+        protected Enum _moveSoundEffect;
+        protected Enum _selectSoundEffect;
+        protected IPlaySfx AsIPlaySfx => this;
         public MainMenuGuiButtonController(GuiComponent parent, Vector2 offset = new Vector2(), Point size = new Point()) : base(parent, offset, size)
         {
             CursorGuiImageComponent =
@@ -30,6 +35,7 @@ namespace MultiplayerZelda.UI.TitleScreen
         public override void Initialize()
         {
             base.Initialize();
+            CursorGuiImageComponent?.Initialize();
             SubScribeToButtons();
         }
 
@@ -39,9 +45,9 @@ namespace MultiplayerZelda.UI.TitleScreen
             CursorGuiImageComponent.LocalPosition = UpdateFingerCursorIcon();
         }
         /// <summary>
-        /// Updates the cursor icon to the correct place on the screen
+        /// Calculates where the cursor icon should be on the screen
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Returns the location that the cursor should go to</returns>
         private Vector2 UpdateFingerCursorIcon()
         {
             var tempLocation = ButtonsToManage[CurrentSelection].GlobalPosition;
@@ -50,6 +56,21 @@ namespace MultiplayerZelda.UI.TitleScreen
             tempLocation.Y += ButtonsToManage[CurrentSelection].Size.Y / 2;
             return tempLocation;
         }
+
+        protected override void SelectButton(int newSelection, bool selectedByMouse = false)
+        {
+            base.SelectButton(newSelection, selectedByMouse);
+            if (!selectedByMouse && _moveSoundEffect != null)
+                AsIPlaySfx.PlaySfx(_moveSoundEffect);
+        }
+
+        protected override void PressButton(int buttonToPress, bool pressedByMouse = false)
+        {
+            base.PressButton(buttonToPress, pressedByMouse);
+            if (_selectSoundEffect != null)
+                AsIPlaySfx.PlaySfx(_selectSoundEffect);
+        }
+
         public void AButtonEventHandler(object sender, ControllerButtons buttonPressed, ButtonActions actionPerformed)
         {
             PressButton(CurrentSelection);
@@ -67,11 +88,10 @@ namespace MultiplayerZelda.UI.TitleScreen
 
         public void SubScribeToButtons()
         {
-            var asISubscribeToButton = (ISubscribeToButton) this;
+            var asISubscribeToButton = (ISubscribeToButton)this;
             asISubscribeToButton.SubscribeToButtonPressed(0, ControllerButtons.A, AButtonEventHandler);
             asISubscribeToButton.SubscribeToButtonPressed(0, ControllerButtons.Up, UpButtonEventHandler);
             asISubscribeToButton.SubscribeToButtonPressed(0, ControllerButtons.Down, DownButtonEventHandler);
-
         }
     }
 }
